f2bab2399f51fc2a304424c10fc470fc70c7a1bb
isbige <1036749024@qq.com>
2019-07-22 23:12:00 +0800

    初始化配置

---
+++ middlewares/check.js
@@ -0,0 +1,14 @@
+module.exports = {
+    checkLogin:function checkLogin(req,res,next){
+        if(!req.session.user){
+            req.flash("error",'未登录');
+            return res.redirect('/signin')
+        }
+    },
+    checkNotLogin:function checkNotLogin(req,res,next){
+        if(req.session.user){
+            req.flash('error','已登录');
+            return res.redirect('back') // 返回之前的页面
+        }
+    }
+}
